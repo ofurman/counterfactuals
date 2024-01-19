4121173aeac72a0c2f21c72bb5f57ac04b49eb6d
Oleksii <fast_enogh@ss.com>
2024-01-19 16:33:24 +0100

    feat: WACH method

---
+++ alternative_methods/wach_exp.py
@@ -0,0 +1,147 @@
+import hydra
+import os
+import mlflow
+from time import time
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import neptune
+from uuid import uuid4
+from joblib import dump, load
+from hydra.utils import instantiate
+from tqdm import tqdm
+
+from omegaconf import DictConfig
+from sklearn.metrics import classification_report
+from alibi.explainers import Counterfactual
+
+from counterfactuals.metrics.metrics import (
+    categorical_distance,
+    continuous_distance,
+    distance_l2_jaccard,
+    distance_mad_hamming,
+    perc_valid_actionable_cf,
+    perc_valid_cf,
+    plausibility,
+    kde_density,
+    sparsity,
+)
+from counterfactuals.utils import add_prefix_to_dict, process_classification_report
+
+
+@hydra.main(config_path="../conf/other_methods", config_name="config_wach", version_base="1.2")
+def main(cfg: DictConfig):
+    tf.compat.v1.disable_eager_execution()
+
+    run = neptune.init_run(
+        mode="async" if cfg.neptune.enable else "offline",
+        project=cfg.neptune.project,
+        api_token=cfg.neptune.api_token,
+        tags=list(cfg.neptune.tags) if "tags" in cfg.neptune else None,
+    )
+
+    output_folder = cfg.experiment.output_folder
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Log parameters using Hydra config
+    run["parameters/experiment"] = cfg.experiment
+    run["parameters/dataset"] = cfg.dataset
+    run["parameters/disc_model"] = cfg.disc_model
+    run["parameters/gen_model"] = "WACH"
+
+    dataset = instantiate(cfg.dataset)
+    X_train, X_test, y_train, y_test = dataset.X_train, dataset.X_test, dataset.y_train, dataset.y_test
+
+    disc_model = instantiate(cfg.disc_model)
+    disc_model.fit(dataset.X_train, dataset.y_train.reshape(-1))
+    report = classification_report(dataset.y_test, disc_model.predict(dataset.X_test), output_dict=True)
+
+    mlflow.log_metrics(process_classification_report(report, prefix="disc_test"))
+    run["metrics"] = process_classification_report(report, prefix="disc_test")
+
+    disc_model_path = os.path.join(output_folder, f"disc_model_{uuid4()}.joblib")
+    dump(disc_model, disc_model_path)
+    run["disc_model"].upload(disc_model_path)
+
+    X_test_pred_path = os.path.join(output_folder, "X_test_pred.csv")
+    pd.DataFrame(disc_model.predict(dataset.X_test)).to_csv(X_test_pred_path, index=False)
+    run["X_test_pred"].upload(X_test_pred_path)
+
+
+    target_proba = 1.0
+    tol = 0.01 # want counterfactuals with p(class)>0.99
+    target_class = 'other' # any class other than origin will do
+    max_iter = 1000
+    lam_init = 1e-1
+    max_lam_steps = 10
+    learning_rate_init = 0.1
+    feature_range = (X_train.min(),X_train.max())
+
+
+    # X_test = X_test[:20]
+    # y_test = y_test[:20]
+    Xs_cfs = []
+    start_time = time()
+    for X, y in tqdm(zip(X_test, y_test), total=len(X_test)):
+        target_class = np.abs(y - 1).flatten().astype(int)[0]
+        print(target_class)
+        X = X.reshape((1,) + X.shape)
+        shape = (1,) + X_train.shape[1:]
+        cf = Counterfactual(disc_model.predict_proba, shape=shape, target_proba=target_proba, tol=tol,
+                            target_class=target_class, max_iter=max_iter, lam_init=lam_init,
+                            max_lam_steps=max_lam_steps, learning_rate_init=learning_rate_init,
+                            feature_range=feature_range)
+        explanation = cf.explain(X)
+        Xs_cfs.append(explanation.cf['X'])
+    run["metrics/avg_time_one_cf"] = (time() - start_time) / X_test.shape[0]
+
+    Xs_cfs = np.array(Xs_cfs).squeeze()
+    counterfactuals_path = os.path.join(output_folder, "counterfactuals.csv")
+    pd.DataFrame(Xs_cfs).to_csv(counterfactuals_path, index=False)
+    run["counterfactuals"].upload(counterfactuals_path)
+
+    ys_cfs_disc_pred = disc_model.predict(Xs_cfs)
+    ys_orig_disc_pred = disc_model.predict(X_test)
+
+    Xs = X_test
+    ys_orig = y_test.flatten()
+
+    metrics = {
+        # "valid_cf_gen_orig": perc_valid_cf(ys_orig, y_cf=ys_cfs_gen_pred),
+        "valid_cf_disc_orig": perc_valid_cf(ys_orig, y_cf=ys_cfs_disc_pred),
+        # "valid_cf_gen": perc_valid_cf(ys_orig_gen_pred, y_cf=ys_cfs_gen_pred),
+        "valid_cf_disc": perc_valid_cf(ys_orig_disc_pred, y_cf=ys_cfs_disc_pred),
+        # "perc_valid_actionable_cf": perc_valid_actionable_cf(X=dataset.X_test[:100], X_cf=Xs_cfs, y=ys_orig_pred, y_cf=ys_cfs_pred,
+        #                                                      actionable_features=dataset.actionable_features),
+        "dissimilarity_proximity_categorical_hamming": categorical_distance(X=Xs, X_cf=Xs_cfs, categorical_features=dataset.categorical_features, metric='hamming', agg='mean'),
+        "dissimilarity_proximity_categorical_jaccard": categorical_distance(X=Xs, X_cf=Xs_cfs, categorical_features=dataset.categorical_features, metric='jaccard', agg='mean'),
+        
+        "dissimilarity_proximity_continuous_manhatan": continuous_distance(X=Xs, X_cf=Xs_cfs, continuous_features=dataset.numerical_features, metric='cityblock', X_all=dataset.X_test),
+        "dissimilarity_proximity_continuous_euclidean": continuous_distance(X=Xs, X_cf=Xs_cfs, continuous_features=dataset.numerical_features, metric='euclidean', X_all=dataset.X_test),
+        "dissimilarity_proximity_continuous_mad": continuous_distance(X=Xs, X_cf=Xs_cfs, continuous_features=dataset.numerical_features, metric='mad', X_all=dataset.X_test),
+
+        "distance_l2_jaccard": distance_l2_jaccard(X=Xs, X_cf=Xs_cfs,
+                                                   continuous_features=dataset.numerical_features, categorical_features=dataset.categorical_features),
+        "distance_mad_hamming": distance_mad_hamming(X=Xs, X_cf=Xs_cfs,
+                                                    continuous_features=dataset.numerical_features, categorical_features=dataset.categorical_features, X_all=Xs, agg='mean'),
+
+        "plausibility": plausibility(
+            Xs, Xs_cfs, ys_orig,
+            continuous_features_all=dataset.numerical_features,
+            categorical_features_all=dataset.categorical_features,
+            X_train=dataset.X_train,
+            ratio_cont=None
+        ),
+
+        # "flow_log_density": np.mean(np.concatenate([log_p_zeros[ys_orig == 0], log_p_ones[ys_orig == 1]])),
+        "kde_log_density": kde_density(dataset.X_train, dataset.y_train, Xs, Xs_cfs, ys_orig),
+
+        "sparsity": sparsity(Xs, Xs_cfs),
+    }
+    run["metrics/cf"] = metrics
+
+    run.stop()
+
+if __name__ == "__main__":
+    main()
+
