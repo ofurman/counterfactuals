1a244245d5dd05ca63e6f16192f3a1af5306fe5a
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2024-01-27 22:38:50 -0100

    Update the approach.

--- counterfactuals/optimizers/approach_gen_disc_loss.py
+++ counterfactuals/optimizers/approach_gen_disc_loss.py
@@ -15,7 +15,8 @@ from counterfactuals.optimizers.base import BaseCounterfactualModel
 
 
 class ApproachGenDiscLoss(BaseCounterfactualModel):
-    def __init__(self, gen_model, disc_model, disc_model_criterion, device=None, neptune_run=None, checkpoint_path=None):
+    def __init__(self, gen_model, disc_model, disc_model_criterion, device=None, neptune_run=None,
+                 checkpoint_path=None):
         self.disc_model_criterion = disc_model_criterion
         super().__init__(gen_model, disc_model, device, neptune_run, checkpoint_path)
 
@@ -30,31 +31,35 @@ class ApproachGenDiscLoss(BaseCounterfactualModel):
         if delta is None:
             raise ValueError("Parameter 'delta' should be in kwargs")
 
-        dist = torch.linalg.norm(x_origin-x_param, axis=1)
+        dist = torch.linalg.norm(x_origin - x_param, axis=1)
 
-        if self.disc_model and (step < 2000):
-            outputs = self.disc_model.forward(x_param)
-            outputs = outputs.reshape(-1) if outputs.shape[0] == 1 else outputs
-            context_target = context_target.reshape(-1) if context_target.shape[0] == 1 else context_target
-            # loss_d = self.disc_model_criterion(outputs, context_target)
-            loss_d = torch.nn.functional.relu((-2 * context_target + 1) * (outputs - torch.Tensor([0.5])))
-        else:
-            loss_d = torch.zeros(1)
+        outputs = self.disc_model.forward(x_param)
+        outputs = outputs.reshape(-1) if outputs.shape[0] == 1 else outputs
+        context_target = context_target.reshape(-1) if context_target.shape[0] == 1 else context_target
+
+        # loss_d = self.disc_model_criterion(outputs, context_target)
+        # loss_d = torch.nn.functional.relu((-2 * context_target + 1) * (outputs - torch.Tensor([0.5])))
+
+        def loss_function(context_target, outputs, eps=0.01):
+            scaled = -2 * context_target + 1
+            return torch.nn.functional.relu(scaled * (outputs - torch.Tensor([0.5]) + scaled * torch.Tensor([eps])))
+
+        loss_d = loss_function(context_target, outputs, eps=0.05)
 
         p_x_param_c_orig = self.gen_model.log_prob(x_param, context=context_origin)
         p_x_param_c_target = self.gen_model.log_prob(x_param, context=context_target)
-        p_x_orig_c_orig = delta # self.gen_model.log_prob(x_origin, context=context_origin.flatten()[0].repeat((x_origin.shape[0], 1)))
+        p_x_orig_c_orig = delta
 
-        p_x_param_c_orig_with_beta = p_x_param_c_orig + beta
-        max_inner = torch.nn.functional.relu(p_x_orig_c_orig-p_x_param_c_target)
+        p_x_param_c_orig_with_beta = p_x_param_c_orig
+        max_inner = torch.nn.functional.relu(p_x_orig_c_orig - p_x_param_c_target)
         max_outer = torch.nn.functional.relu(p_x_param_c_orig_with_beta - p_x_param_c_target)
-        loss = dist + alpha * (max_outer + 100*max_inner + loss_d)
+        loss = dist + alpha * max_inner + alpha * loss_d
         return loss, dist, max_inner, max_outer, loss_d
 
     def generate_counterfactuals(self, Xs, ys, epochs, lr, alpha, beta):
         Xs = Xs[:, np.newaxis, :]
         ys = ys.reshape(-1, 1)
-        ys_hat = np.abs(1-ys).reshape(-1, 1)
+        ys_hat = np.abs(1 - ys).reshape(-1, 1)
         x_cfs = []
         for X, y, y_hat in tqdm(zip(Xs, ys, ys_hat)):
             X = torch.Tensor(X)
