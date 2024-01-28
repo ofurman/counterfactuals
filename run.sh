58f6d080f7740024a4600c59cec3b0d1200d1e0c
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2024-01-28 11:27:55 -0100

    Update.

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 python generative_model_exp.py --multirun \
-    dataset._target_=counterfactuals.datasets.MoonsDataset #,counterfactuals.datasets.LawDataset,counterfactuals.datasets.HelocDataset
+    dataset._target_=counterfactuals.datasets.MoonsDataset,counterfactuals.datasets.LawDataset,counterfactuals.datasets.HelocDataset \
+    disc_model.model=LR #,MLP
