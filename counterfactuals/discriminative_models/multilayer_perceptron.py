806e15ed85a7fbca23fec30fe0022f9e9ec7d1e0
Oleksii Furman <oleksii.furman@gmail.com>
2024-04-20 14:32:00 +0200

    ppcef node results and models

--- counterfactuals/discriminative_models/multilayer_perceptron.py
+++ counterfactuals/discriminative_models/multilayer_perceptron.py
@@ -21,12 +21,15 @@ class MultilayerPerceptron(BaseDiscModel):
             self.layers.append(torch.nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
         self.relu = torch.nn.ReLU()
         self.dropout = torch.nn.Dropout(dropout)
+        self.target_size = target_size
         if target_size == 1:
             self.final_activation = torch.nn.Sigmoid()
             self.criterion = torch.nn.BCEWithLogitsLoss()
+            self.prep_for_loss = lambda x: x.view(-1, 1).float()
         else:
             self.final_activation = torch.nn.Softmax(dim=1)
             self.criterion = torch.nn.CrossEntropyLoss()
+            self.prep_for_loss = lambda x: x.view(-1).long()
 
     def forward(self, x):
         for i in range(len(self.layers)):
@@ -36,53 +39,67 @@ class MultilayerPerceptron(BaseDiscModel):
                 x = self.relu(self.dropout(self.layers[i](x)))
         return x
 
-    def fit(self, train_loader, test_loader=None, epochs=200, lr=0.001):
+    def fit(
+        self,
+        train_loader,
+        test_loader=None,
+        epochs=200,
+        lr=0.001,
+        patience: int = 20,
+        eps: float = 1e-3,
+        checkpoint_path: str = "best_model.pth",
+    ):
+        min_test_loss = float("inf")
         optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         for epoch in (pbar := tqdm(range(epochs))):
-            losses = []
-            test_losses = []
+            train_loss = 0
+            test_loss = 0
             for i, (examples, labels) in enumerate(train_loader):
-                labels = labels.type(torch.int64)
                 optimizer.zero_grad()
                 outputs = self.forward(examples)
-                labels = labels.reshape(-1).type(torch.int64)
-                loss = self.criterion(outputs, labels)
-                losses.append(loss.item())
+                loss = self.criterion(outputs, self.prep_for_loss(labels))
+                train_loss += loss.item()
                 loss.backward()
                 optimizer.step()
+            train_loss /= len(train_loader)
             if test_loader:
                 with torch.no_grad():
                     for i, (examples, labels) in enumerate(test_loader):
-                        labels = labels.type(torch.int64)
                         outputs = self.forward(examples)
-                        loss = self.criterion(outputs, labels)
-                        test_losses.append(loss.item())
+                        loss = self.criterion(outputs, self.prep_for_loss(labels))
+                        test_loss += loss.item()
                         # Early stopping
-            if epoch > 10 and np.mean(test_losses[-10:]) > np.mean(test_losses[-5:]):
-                break
+                    test_loss /= len(test_loader)
+                if test_loss < (min_test_loss + eps):
+                    min_test_loss = test_loss
+                    patience_counter = 0
+                    self.save(checkpoint_path)
+                else:
+                    patience_counter += 1
+                if patience_counter > patience:
+                    break
+                self.load(checkpoint_path)
             pbar.set_description(
-                f"Epoch {epoch}, Train Loss: {np.mean(losses):.4f}, Test Loss: {np.mean(test_losses):.4f}"
+                f"Epoch {epoch}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}"
             )
 
     def predict(self, X_test):
         if isinstance(X_test, np.ndarray):
-            X_test = torch.from_numpy(X_test)
+            X_test = torch.from_numpy(X_test).float()
         with torch.no_grad():
-            probs = torch.from_numpy(self.predict_proba(X_test))
-            if self.target_size > 1:
-                probs = torch.argmax(probs, dim=1)
-            else:
-                probs = probs > 0.5
-            return np.squeeze(probs)
+            probs = self.predict_proba(X_test)
+            probs = torch.argmax(probs, dim=1)
+            return probs.squeeze().float()
 
     def predict_proba(self, X_test):
         if isinstance(X_test, np.ndarray):
-            X_test = torch.from_numpy(X_test)
+            X_test = torch.from_numpy(X_test).float()
         with torch.no_grad():
-            logits = self.forward(X_test.type(torch.float32))
+            logits = self.forward(X_test)
             probs = self.final_activation(logits)
-            # probs = torch.hstack([1 - probs, probs]).detach().numpy().astype(np.float32)
-            return probs.type(torch.float32).numpy()
+            if self.target_size == 1:
+                probs = torch.hstack([1 - probs, probs])
+            return probs.float()
 
     def save(self, path):
         torch.save(self.state_dict(), path)
