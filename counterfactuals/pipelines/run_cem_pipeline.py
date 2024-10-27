1088184a44a57cb9a62564ea281dce1ea99f8b76
Oleksii <fast_enogh@ss.com>
2024-10-27 18:39:30 +0100

    feat: CEM

---
+++ counterfactuals/pipelines/run_cem_pipeline.py
@@ -0,0 +1,188 @@
+import logging
+import os
+import hydra
+import numpy as np
+import pandas as pd
+from time import time
+import torch
+import neptune
+from neptune.utils import stringify_unsupported
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+import torch.utils
+
+from counterfactuals.metrics.metrics import evaluate_cf
+from counterfactuals.cf_methods.cem.cem import CEM_CF
+from counterfactuals.pipelines.nodes.helper_nodes import log_parameters, set_model_paths
+from counterfactuals.pipelines.nodes.disc_model_nodes import create_disc_model
+from counterfactuals.pipelines.nodes.gen_model_nodes import create_gen_model
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+
+def search_counterfactuals(
+    cfg: DictConfig,
+    dataset: DictConfig,
+    gen_model: torch.nn.Module,
+    disc_model: torch.nn.Module,
+    run: neptune.Run,
+    save_folder: str,
+) -> torch.nn.Module:
+    """
+    Create counterfactuals using CEM method
+    """
+    cf_method_name = "CEM"
+    disc_model_name = cfg.disc_model.model._target_.split(".")[-1]
+
+    logger.info("Filtering out target class data for counterfactual generation")
+    target_class = cfg.counterfactuals_params.target_class
+    X_test_origin = dataset.X_test[dataset.y_test != target_class]
+    y_test_origin = dataset.y_test[dataset.y_test != target_class]
+
+    logger.info("Creating counterfactual model")
+    cf_method = CEM_CF(
+        disc_model=disc_model,
+        mode=cfg.counterfactuals_params.mode,
+        kappa=cfg.counterfactuals_params.kappa,
+        beta=cfg.counterfactuals_params.beta,
+        c_init=cfg.counterfactuals_params.c_init,
+        c_steps=cfg.counterfactuals_params.c_steps,
+        max_iterations=cfg.counterfactuals_params.max_iterations,
+        learning_rate_init=cfg.counterfactuals_params.learning_rate_init,
+    )
+
+    logger.info("Calculating log_prob_threshold")
+    train_dataloader_for_log_prob = dataset.train_dataloader(
+        batch_size=cfg.counterfactuals_params.batch_size, shuffle=False
+    )
+    log_prob_threshold = torch.quantile(
+        gen_model.predict_log_prob(train_dataloader_for_log_prob),
+        cfg.counterfactuals_params.log_prob_quantile,
+    )
+    run["parameters/log_prob_threshold"] = log_prob_threshold
+    logger.info(f"log_prob_threshold: {log_prob_threshold:.4f}")
+
+    logger.info("Handling counterfactual generation")
+    cf_dataloader = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(
+            torch.tensor(X_test_origin).float(),
+            torch.tensor(y_test_origin).float(),
+        ),
+        batch_size=cfg.counterfactuals_params.batch_size,
+        shuffle=False,
+    )
+
+    time_start = time()
+    Xs_cfs, Xs, ys_orig, ys_target, model_returned = cf_method.explain_dataloader(
+        dataloader=cf_dataloader, target_class=target_class
+    )
+
+    cf_search_time = np.mean(time() - time_start)
+    run["metrics/cf_search_time"] = cf_search_time
+
+    counterfactuals_path = os.path.join(
+        save_folder, f"counterfactuals_{cf_method_name}_{disc_model_name}.csv"
+    )
+    pd.DataFrame(Xs_cfs).to_csv(counterfactuals_path, index=False)
+    run["counterfactuals"].upload(counterfactuals_path)
+
+    return Xs_cfs, Xs, log_prob_threshold, ys_orig, ys_target, model_returned
+
+
+def calculate_metrics(
+    gen_model: torch.nn.Module,
+    disc_model: torch.nn.Module,
+    Xs_cfs: np.ndarray,
+    model_returned: np.ndarray,
+    categorical_features: list,
+    continuous_features: list,
+    X_train: np.ndarray,
+    y_train: np.ndarray,
+    X_test: np.ndarray,
+    y_test: np.ndarray,
+    median_log_prob: float,
+    run: neptune.Run,
+):
+    """
+    Calculate metrics for counterfactuals
+    """
+    logger.info("Calculating metrics")
+    metrics = evaluate_cf(
+        gen_model=gen_model,
+        disc_model=disc_model,
+        X_cf=Xs_cfs,
+        model_returned=model_returned,
+        categorical_features=categorical_features,
+        continuous_features=continuous_features,
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+        median_log_prob=median_log_prob,
+    )
+    run["metrics/cf"] = stringify_unsupported(metrics)
+    logger.info(f"Metrics:\n{stringify_unsupported(metrics)}")
+    return metrics
+
+
+@hydra.main(config_path="./conf", config_name="cem_config", version_base="1.2")
+def main(cfg: DictConfig):
+    torch.manual_seed(0)
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    logger.info("Initializing Neptune run")
+    run = neptune.init_run(
+        mode="async" if cfg.neptune.enable else "offline",
+        project=cfg.neptune.project,
+        api_token=cfg.neptune.api_token,
+        tags=list(cfg.neptune.tags) if "tags" in cfg.neptune else None,
+    )
+
+    log_parameters(cfg, run)
+
+    logger.info("Loading dataset")
+    dataset = instantiate(cfg.dataset)
+
+    for fold_n, _ in enumerate(dataset.get_cv_splits(5)):
+        disc_model_path, gen_model_path, save_folder = set_model_paths(cfg, fold=fold_n)
+        disc_model = create_disc_model(cfg, dataset, disc_model_path, save_folder, run)
+
+        if cfg.experiment.relabel_with_disc_model:
+            dataset.y_train = disc_model.predict(dataset.X_train).detach().numpy()
+            dataset.y_test = disc_model.predict(dataset.X_test).detach().numpy()
+
+        gen_model = create_gen_model(cfg, dataset, gen_model_path, run)
+
+        Xs_cfs, Xs, log_prob_threshold, ys_orig, ys_target, model_returned = (
+            search_counterfactuals(
+                cfg, dataset, gen_model, disc_model, run, save_folder
+            )
+        )
+
+        metrics = calculate_metrics(
+            gen_model=gen_model,
+            disc_model=disc_model,
+            Xs_cfs=Xs_cfs,
+            model_returned=model_returned,
+            categorical_features=dataset.categorical_features,
+            continuous_features=dataset.numerical_features,
+            X_train=dataset.X_train,
+            y_train=dataset.y_train.reshape(-1),
+            X_test=Xs,
+            y_test=ys_orig,
+            median_log_prob=log_prob_threshold,
+            run=run,
+        )
+
+        run[f"metrics/cf/fold_{fold_n}"] = stringify_unsupported(metrics)
+        df_metrics = pd.DataFrame(metrics, index=[0])
+        df_metrics.to_csv(os.path.join(save_folder, "cf_metrics.csv"), index=False)
+
+    run.stop()
+
+
+if __name__ == "__main__":
+    main()
