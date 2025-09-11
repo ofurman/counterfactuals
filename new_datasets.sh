229803de4ae80496bd9972cb4fc309569bae20ca
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2025-09-11 18:15:40 +0200

    experiment script

--- new_datasets.sh
+++ new_datasets.sh
@@ -1,7 +1,24 @@
 # python counterfactuals/pipelines/run_ppcef_pipeline.py -m \
-#     dataset._target_=counterfactuals.datasets.LendingClubDataset,counterfactuals.datasets.BankMarketingDataset,counterfactuals.datasets.GiveMeSomeCreditDataset \
+#     dataset._target_=counterfactuals.datasets.LendingClubDataset,counterfactuals.datasets.BankMarketingDataset,counterfactuals.datasets.GiveMeSomeCreditDataset,counterfactuals.datasets.AdultCensusDataset \
 #     disc_model=mlp,mlr
 
-python counterfactuals/pipelines/run_cchvae_pipeline.py -m \
-    dataset._target_=counterfactuals.datasets.LendingClubDataset,counterfactuals.datasets.BankMarketingDataset \
+python counterfactuals/pipelines/run_dice_pipeline.py -m \
+    dataset._target_=counterfactuals.datasets.GiveMeSomeCreditDataset \
     disc_model=mlp,mlr
+
+# python counterfactuals/pipelines/run_cchvae_pipeline.py -m \
+#     dataset._target_=counterfactuals.datasets.AdultCensusDataset \
+#     disc_model=mlp,mlr
+
+#python counterfactuals/pipelines/run_cchvae_pipeline.py -m \
+#    dataset._target_=counterfactuals.datasets.LendingClubDataset,counterfactuals.datasets.BankMarketingDataset \
+#    disc_model=mlp,mlr
+
+
+# python counterfactuals/pipelines/run_ppcef_pipeline.py -m \
+#      dataset._target_=counterfactuals.datasets.LawDataset,counterfactuals.datasets.GermanCreditDataset,counterfactuals.datasets.AdultDataset \
+#      disc_model=mlp,mlr
+
+# python counterfactuals/pipelines/run_cchvae_pipeline.py -m \
+#     dataset._target_=counterfactuals.datasets.LawDataset,counterfactuals.datasets.GermanCreditDataset,counterfactuals.datasets.AdultDataset \
+#     disc_model=mlp,mlr
