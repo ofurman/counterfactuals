baeb672528afe3cc2ade4a362792532fe10cc8e5
marcelele-0 <mnem12321@gmail.com>
2025-10-02 23:11:37 +0200

    refactor: restructure cf_methods with base classes and mixins

---
+++ counterfactuals/cf_methods/group/__init__.py
@@ -0,0 +1,7 @@
+"""Group counterfactual methods package."""
+
+from .group_ppcef.rppcef import RPPCEF
+
+__all__ = [
+    "RPPCEF",
+]
---
+++ counterfactuals/cf_methods/local/__init__.py
@@ -0,0 +1,31 @@
+"""Local counterfactual methods package."""
+
+from .ares.ares import ARES
+from .artelt.artelt import Artelt
+from .casebased_sace.casebased_sace import CaseBasedSACE
+from .cegp.cegp import CEGP
+from .cem.cem import CEM_CF
+from .cet.cet import CET
+from .dice.dice import DiceExplainerWrapper
+from .lice.lice import LiCE
+from .ppcef.ppcef import PPCEF
+from .regression_ppcef.ppcefr import PPCEFR
+from .sace.sace import SACE
+from .wach.wach import WACH
+from .wach.wach_ours import WACH_OURS
+
+__all__ = [
+    "PPCEF",
+    "WACH",
+    "WACH_OURS",
+    "Artelt",
+    "PPCEFR",
+    "DiceExplainerWrapper",
+    "CEM_CF",
+    "ARES",
+    "SACE",
+    "LiCE",
+    "CET",
+    "CEGP",
+    "CaseBasedSACE",
+]
