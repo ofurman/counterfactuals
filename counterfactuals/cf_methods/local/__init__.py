a373c4b059cc43a8cc0bdfbb673639685e3ffa16
marcelele-0 <mnem12321@gmail.com>
2025-10-10 00:22:58 +0200

    refactor: simplify local counterfactual methods package by removing optional import handling

--- counterfactuals/cf_methods/local/__init__.py
+++ counterfactuals/cf_methods/local/__init__.py
@@ -1,50 +1,31 @@
-"""Local counterfactual methods package.
+"""Local counterfactual methods package."""
 
-This module exposes local counterfactual method classes but guards
-optional, heavy dependencies so that a simple import doesn't fail
-when optional packages are not installed.
-"""
+from .ares.ares import AReS as ARES
+from .artelt.artelt import Artelt
+from .casebased_sace.casebased_sace import CaseBasedSACE
+from .cegp.cegp import CEGP
+from .cem.cem import CEM_CF
+from .cet.cet import CounterfactualExplanationTree as CET
+from .dice.dice import DiceExplainerWrapper
+from .lice.lice import LiCE
+from .ppcef.ppcef import PPCEF
+from .regression_ppcef.ppcefr import PPCEFR
+from .sace.sace import SACE
+from .wach.wach import WACH
+from .wach.wach_ours import WACH_OURS
 
-import importlib
-import warnings
-
-__all__ = []
-
-
-def _optional_from(module: str, name: str, alias: str | None = None):
-    """Try to import ``name`` from ``module`` and expose it in this
-    package globals. If import fails, set the name to None and warn.
-    """
-    as_name = alias or name
-    try:
-        mod = importlib.import_module(module, package=__package__)
-        obj = getattr(mod, name)
-        globals()[as_name] = obj
-        __all__.append(as_name)
-    except Exception as e:
-        globals()[as_name] = None
-        warnings.warn(
-            f"Optional counterfactual method '{as_name}' could not be imported "
-            f"from '{module}': {e}. Install optional dependencies to enable it.",
-            ImportWarning,
-        )
-
-
-# Core/lightweight methods - prefer these to be available without extras
-_optional_from(".ppcef.ppcef", "PPCEF")
-_optional_from(".wach.wach", "WACH")
-_optional_from(".wach.wach_ours", "WACH_OURS")
-_optional_from(".artelt.artelt", "Artelt")
-_optional_from(".regression_ppcef.ppcefr", "PPCEFR")
-
-# Other local methods (may have optional deps)
-_optional_from(".dice.dice", "DiceExplainerWrapper")
-_optional_from(".cem.cem", "CEM_CF")
-_optional_from(".ares.ares", "AReS", alias="ARES")
-_optional_from(".sace.sace", "SACE")
-_optional_from(".cet.cet", "CounterfactualExplanationTree", alias="CET")
-_optional_from(".casebased_sace.casebased_sace", "CaseBasedSACE")
-
-# Methods with heavy optional dependencies
-_optional_from(".cegp.cegp", "CEGP")  # needs alibi/tensorflow
-_optional_from(".lice.lice", "LiCE")  # needs spn package
+__all__ = [
+    "PPCEF",
+    "WACH",
+    "WACH_OURS",
+    "Artelt",
+    "PPCEFR",
+    "DiceExplainerWrapper",
+    "CEM_CF",
+    "ARES",
+    "SACE",
+    "LiCE",
+    "CET",
+    "CEGP",
+    "CaseBasedSACE",
+]
