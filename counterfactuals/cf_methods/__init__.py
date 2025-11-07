c8ed43e294c6d216575972ea8744ae2cd9fe264d
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2025-11-07 15:08:36 +0100

    refactor: rename modules to resolve python keyword conflicts

--- counterfactuals/cf_methods/__init__.py
+++ counterfactuals/cf_methods/__init__.py
@@ -1,15 +1,14 @@
 # Import base classes and mixins
 # Import global methods using importlib to avoid keyword conflict
-import importlib
-
 from .counterfactual_base import BaseCounterfactualMethod, ExplanationResult
 from .global_counterfactual_mixin import GlobalCounterfactualMixin
-from .group import RPPCEF
+from .global_methods import GLOBE_CE, AReS
 from .group_counterfactual_mixin import GroupCounterfactualMixin
+from .group_methods import GLANCE, RPPCEF
+from .local_counterfactual_mixin import LocalCounterfactualMixin
 
 # Import from subpackages
-from .local import CCHVAE, DICE, PPCEF, PPCEFR, WACH, WACH_OURS, Artelt
-from .local_counterfactual_mixin import LocalCounterfactualMixin
+from .local_methods import CCHVAE, DICE, PPCEF, PPCEFR, WACH, WACH_OURS, Artelt
 
 __all__ = [
     # Base classes
@@ -25,10 +24,11 @@ __all__ = [
     "WACH_OURS",
     "Artelt",
     "PPCEFR",
-    # Global methods
     "CCHVAE",
-    "GlobalGLANCE",
+    # Global methods
+    "AReS",
     "GLOBE_CE",
     # Group methods
     "RPPCEF",
+    "GLANCE",
 ]
--- counterfactuals/cf_methods/global_methods/__init__.py
+++ counterfactuals/cf_methods/global_methods/__init__.py
@@ -1,7 +1,9 @@
 """Global counterfactual methods package."""
 
+from .ares.ares import AReS
 from .globe_ce.globe_ce import GLOBE_CE
 
 __all__ = [
     "GLOBE_CE",
+    "AReS",
 ]
